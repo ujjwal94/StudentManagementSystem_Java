8188acd536f1bb7c54fea0b01d8d641357c0311e
Ujjwal Roy <ujjwal94@gmail.com>
2019-07-06 12:43:31 +0530

    Publishing final version

---
+++ src/MySqlConn.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+import javax.swing.*;
+
+public class MySqlConn 
+{
+    Connection conn=null;
+    public static Connection ConnectDB()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=convertToNull","root","hr");
+            
+            return conn;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+    
+    
+}
